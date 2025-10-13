b856aab7fd914d765a91f70ef28737032f20c370
[dyad] <joaquimcruz3110@gmail.com>
2025-10-13 12:55:53 -0300

    [dyad] Adicionei um `console.log` na função `formatCurrency` para depuração e solicitei que você verifique o console e inspecione o elemento do preço na página "Explorar Produtos". - wrote 1 file(s)

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,8 +1,10 @@
 export const formatCurrency = (value: number): string => {
-  return new Intl.NumberFormat('pt-BR', {
+  const formattedValue = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(value);
+  console.log(`formatCurrency received: ${value}, returned: ${formattedValue}`); // Temporário para depuração
+  return formattedValue;
 };
