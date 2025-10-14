d34e5275f64d46928e6659d89323f15e3995b299
[dyad] <joaquimcruz3110@gmail.com>
2025-10-14 12:20:54 -0300

    [dyad] Corrigindo importação do Mercado Pago nas Edge Functions - wrote 2 file(s)

--- supabase/functions/mercadopago-webhook/index.ts
+++ supabase/functions/mercadopago-webhook/index.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 // @ts-ignore
-import { default as mercadopago } from 'https://esm.sh/mercadopago@2.0.10?target=deno';
+import * as mercadopago from 'https://esm.sh/mercadopago@2.0.10?target=deno'; // Alterado para importar como namespace
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
