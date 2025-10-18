b4aa84460c1a515d11e501ca668fcecd61934f64
[dyad] <joaquimcruz3110@gmail.com>
2025-10-18 13:12:19 -0300

    [dyad] Removi o menu de navegação lateral e ajustei o layout para usar apenas o menu superior, ocupando toda a largura da tela. - wrote 1 file(s), deleted 1 file(s)

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import Header from './Header';
-import Sidebar from './Sidebar';
 import { MadeWithDyad } from './made-with-dyad';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <Sidebar />
-      <div className="flex flex-col">
-        <Header />
-        <main className="flex-grow p-4 bg-dyad-light-gray">
-          <Outlet /> {/* Renderiza o conteúdo específico da rota aqui */}
-        </main>
-        <MadeWithDyad />
-      </div>
+    <div className="flex flex-col min-h-screen w-full"> {/* Removido o grid layout */}
+      <Header />
+      <main className="flex-grow p-4 bg-dyad-light-gray">
+        <Outlet /> {/* Renderiza o conteúdo específico da rota aqui */}
+      </main>
+      <MadeWithDyad />
     </div>
   );
 };
